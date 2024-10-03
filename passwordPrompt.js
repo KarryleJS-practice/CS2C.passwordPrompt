b57278223a103b9f87de776a9d1519f7a839047d
KarryleJS-practice <karrylelagrimas6@gmail.com>
2024-10-03 22:51:30 +0800

    passwordPrompt.js

---
+++ passwordPrompt.js
@@ -0,0 +1,8 @@
+let correctPassword = "Karrylelagrimas0000";
+
+let password;
+do {
+ password = prompt("Enter your password:"   );
+
+} while (password !== correctPassword);
+console.log("Access granted");
